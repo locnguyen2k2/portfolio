5c589b24651521ba52751ee4092a4a3b1c7580ca
Loc Nguyen <locnguyen071102@gmail.com>
2025-02-08 19:43:37 +0700

    refector: update codes

--- src/components/templates/BaseLayout.js
+++ src/components/templates/BaseLayout.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import {Context} from '../../Context';
 import Footer from "../molecules/Footer/Footer";
 import {Outlet, useLocation} from "react-router-dom";
@@ -10,38 +9,38 @@ export default function BaseLayout() {
     const location = useLocation();
     const [currentRoute, setCurrentRoute] = useState(location.pathname);
 
-    const exams = () => {
-        const formData = new FormData();
-        formData.append('operations', JSON.stringify({query: `mutation Login{login(loginArgs: {email: "teacher@ctuet.edu.vn", password: "teacher@123"}){access_token}}`}),);
-        formData.append('map', JSON.stringify({}));
-        try {
-            axios({
-                method: 'post',
-                url: 'https://exam-management-system-z76m.onrender.com/graphql',
-                headers: {'apollo-require-preflight': true, 'content-type': 'multipart/form-data'},
-                data: formData,
-            });
-        } catch (e) {
-            console.log('Preparing for Q&E Management System ...')
-        }
-    }
+    // const exams = () => {
+    //     const formData = new FormData();
+    //     formData.append('operations', JSON.stringify({query: `mutation Login{login(loginArgs: {email: "teacher@ctuet.edu.vn", password: "teacher@123"}){access_token}}`}),);
+    //     formData.append('map', JSON.stringify({}));
+    //     try {
+    //         axios({
+    //             method: 'post',
+    //             url: 'https://exam-management-system-z76m.onrender.com/graphql',
+    //             headers: {'apollo-require-preflight': true, 'content-type': 'multipart/form-data'},
+    //             data: formData,
+    //         });
+    //     } catch (e) {
+    //         console.log('Preparing for Q&E Management System ...')
+    //     }
+    // }
 
-    const labs = () => {
-        try {
-            axios.get('https://laboratory-management-system.onrender.com/apis/categories');
-        } catch (error) {
-            console.log('Prepared for LIMS Management System ...')
-        }
-    }
+    // const labs = () => {
+    //     try {
+    //         axios.get('https://laboratory-management-system.onrender.com/apis/categories');
+    //     } catch (error) {
+    //         console.log('Prepared for LIMS Management System ...')
+    //     }
+    // }
 
-    useEffect(() => {
-        setTimeout(() => {
-            labs()
-        }, 20000);
-        setTimeout(() => {
-            exams()
-        }, 20000)
-    });
+    // useEffect(() => {
+    // setTimeout(() => {
+    //     labs()
+    // }, 20000);
+    // setTimeout(() => {
+    //     exams()
+    // }, 20000)
+    // });
 
     useEffect(() => {
         if (location.pathname !== currentRoute) {
